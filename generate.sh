5a346cb5cff59b3e5fe2d7f9af8d4ffe6ef85afa
Marcin Parafiniuk <mp347235@students.mimuw.edu.pl>
2019-11-26 00:09:20 +0100

    non blocking ws echo server

---
+++ generate.sh
@@ -0,0 +1,6 @@
+#!/bin/bash
+flatc --ts  --no-fb-import -o ./client/src ./deadfish.fbs
+flatc --rust -o ./server/src ./deadfish.fbs
+
+echo -e "import { flatbuffers } from 'flatbuffers';\n$(cat client/src/deadfish_generated.ts)" > client/src/deadfish_generated.ts
+
--- server/src/main.rs
+++ server/src/main.rs
@@ -1,15 +1,35 @@
-extern crate ws;
-
-use ws::listen;
+use websocket::server::sync::Server;
+use websocket::Message;
+use std::{thread, time};
 
 fn main() {
-    println!("Hello, world!");
+    let mut server = Server::bind("127.0.0.1:63987").unwrap();
 
-    // A WebSocket echo server
-    listen("127.0.0.1:63987", |out| {
-        move |msg| {
-            println!("received: {}", msg);
-            out.send(msg)
+    // Set the server to non-blocking.
+    server.set_nonblocking(true).unwrap();
+
+    let hundred_ms = time::Duration::from_millis(100);
+
+    let mut clients = Vec::new();
+
+    loop {
+        match server.accept() {
+            Ok(wsupgrade) => {
+                // Do something with the established TcpStream.
+                println!("CONN!!!");
+                let client = wsupgrade.accept().unwrap();
+                clients.push(client);
+            }
+            _ => {
+                // Nobody tried to connect, move on.
+            }
+        };
+        // println!("no siema");
+        thread::sleep(hundred_ms);
+        for client in &mut clients {
+            let data = client.recv_dataframe().unwrap();
+            println!("payload len {}", data.data.len());
+            client.send_message(&Message::binary(data.data)).unwrap();
         }
-    }).unwrap()
+    }
 }
