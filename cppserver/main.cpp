ebed298d0a8df54abbdcbeedf629ba7460676f2f
Marcin Parafiniuk <marcin.parafiniuk@codilime.com>
2020-01-19 20:01:13 +0100

    m

---
+++ cppserver/main.cpp
@@ -0,0 +1,25 @@
+#include <iostream>
+
+#include <websocketpp/config/asio_no_tls.hpp>
+#include <websocketpp/server.hpp>
+
+#include "deadfish_generated.h"
+
+typedef websocketpp::server<websocketpp::config::asio> server;
+
+void on_message(websocketpp::connection_hdl hdl, server::message_ptr msg) {
+        std::cout << msg->get_payload() << std::endl;
+        
+}
+
+int main() {
+    server print_server;
+
+    print_server.set_message_handler(&on_message);
+
+    print_server.init_asio();
+    print_server.listen(63987);
+    print_server.start_accept();
+
+    print_server.run();
+}
