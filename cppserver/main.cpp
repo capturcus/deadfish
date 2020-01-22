56f4c30dd63f0c6591c05864b0c0ce45f0a24cad
Marcin Parafiniuk <mp347235@students.mimuw.edu.pl>
2020-01-22 01:42:49 +0100

    cpp fb + fbs changes

--- cppserver/main.cpp
+++ cppserver/main.cpp
@@ -8,8 +8,10 @@
 typedef websocketpp::server<websocketpp::config::asio> server;
 
 void on_message(websocketpp::connection_hdl hdl, server::message_ptr msg) {
-        std::cout << msg->get_payload() << std::endl;
-        
+    std::cout << msg->get_payload() << std::endl;
+    const auto payload = msg->get_payload();
+    const auto parsed = flatbuffers::GetRoot<DeadFish::CommandMove>(payload.c_str());
+    std::cout << parsed->target()->x() << ", " << parsed->target()->y() << std::endl;
 }
 
 int main() {
