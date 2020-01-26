dd3758821217e926e986846fba90b38155f78dbf
Marcin Parafiniuk <mp347235@students.mimuw.edu.pl>
2020-01-26 17:52:40 +0100

    delete rust code

--- generate.sh
+++ generate.sh
@@ -3,8 +3,7 @@
 set -e
 
 flatc --ts  --no-fb-import -o ./client/src ./deadfish.fbs
-flatc --rust -o ./server/src ./deadfish.fbs
-flatc --cpp -o ./cppserver ./deadfish.fbs
+flatc --cpp -o ./server ./deadfish.fbs
 
 echo -e "import { flatbuffers } from 'flatbuffers';\n$(cat client/src/deadfish_generated.ts)" > client/src/deadfish_generated.ts
 
