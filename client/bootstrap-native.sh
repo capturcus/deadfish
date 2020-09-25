8e71342e6f2a25c57cb4e3d2b20a2f06d664d1fc
Marcin Parafiniuk <marcin.parafiniuk@gmail.com>
2020-09-25 18:34:10 +0200

    m

--- client/bootstrap-native.sh
+++ client/bootstrap-native.sh
@@ -8,11 +8,4 @@ cmake --build nCine-libraries-native
 cmake -DCMAKE_BUILD_TYPE=Debug -S nCine -B nCine-native -D CMAKE_PREFIX_PATH=$(pwd)/nCine-external
 cmake --build nCine-native
 
-cmake -DCMAKE_BUILD_TYPE=Debug -S dfclient -B dfclient-native
-cmake --build dfclient-native
-
-cp ./nCine-native/libncine_d.so dfclient-native
-cp ./nCine-external/lib/libGLEWd.so.2.2 dfclient-native/
-cp ./nCine-external/lib/libglfw.so.3 dfclient-native/
-cp ./nCine-external/lib/libpng16d.so.16 dfclient-native/
-cp ./nCine-external/lib/libwebp.so.7 dfclient-native/
+./recmake_dfclient_native.sh
--- client/recmake_dfclient_native.sh
+++ client/recmake_dfclient_native.sh
@@ -2,6 +2,12 @@
 rm -rf dfclient-native
 
 cmake -DCMAKE_BUILD_TYPE=Debug -S dfclient -B dfclient-native
-cd dfclient-native
-make -j4
+cmake --build dfclient-native
 
+cp ./nCine-native/libncine_d.so dfclient-native
+cp ./nCine-external/lib/libGLEWd.so.2.2 dfclient-native/
+cp ./nCine-external/lib/libglfw.so.3 dfclient-native/
+cp ./nCine-external/lib/libpng16d.so.16 dfclient-native/
+cp ./nCine-external/lib/libwebp.so.7 dfclient-native/
+
+ln -s ../../data dfclient-native
