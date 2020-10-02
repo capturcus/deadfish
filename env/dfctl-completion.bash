2388c469720ef1a53ea15ded01d470f5db2e3072
Michał Parafiniuk <33375219+mparafin@users.noreply.github.com>
2020-10-02 12:09:55 +0200

    Update dfctl-completion.bash

--- env/dfctl-completion.bash
+++ env/dfctl-completion.bash
@@ -1,1 +1,1 @@
-complete -W "startct stopct ct ctroot server clientnative clientwasm test makeserver makewasm makenative recmakeserver recmakewasm recmakenative debs debc" dfctl
+complete -W "startct stopct ct ctroot server clientnative clientwasm test levelpacker makeserver makewasm makenative recmakeserver recmakewasm recmakenative debs debc" dfctl
