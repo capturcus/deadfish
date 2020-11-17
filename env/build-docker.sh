9e17bb7557959fb683630db4936924e33f1d7334
Marcin Parafiniuk <marcin.parafiniuk@gmail.com>
2020-11-17 21:44:33 +0100

    making a server docker image

--- env/build-docker.sh
+++ env/build-docker.sh
@@ -2,4 +2,4 @@
 docker stop deadfish-env
 docker rm deadfish-env
 docker rmi df-dev-env
-docker build --tag df-dev-env .
+docker build --tag df-dev-env df-dev-env
---
+++ env/build-server-prod-docker.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+
+set -e
+
+docker exec --user ubuntu -it deadfish-env bash -c 'cd /deadfish/levelpacker && ./levelpacker.py && cd /deadfish/server && mkdir -p build-prod && cd build-prod && make -j && cp /usr/lib/x86_64-linux-gnu/libBox2D.so.2.3.0 . && cp /usr/local/lib/libboost_system.so.1.74.0 . && cp /usr/local/lib/libboost_program_options.so.1.74.0 .'
+
+cd ../server/build-prod
+cp deadfishserver ../../env/deadfish-server
+cp *.so.* ../../env/deadfish-server
+cd ..
+cd ../env/deadfish-server
+rm -rf levels
+mkdir levels
+cp ../../levels/*.bin levels
+
+docker rmi deadfish-server
+docker build --tag deadfish-server .
